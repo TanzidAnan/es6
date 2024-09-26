cf5083e712166d4be5319963f6e4d043c17ac71f
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-26 17:42:53 +0600

    arrow object

--- more-arrow.js
+++ more-arrow.js
@@ -1,3 +1,8 @@
 const different =(a,b) =>a-b;
 const diff =different(15,10);
-console.log(diff)
+// console.log(diff)
+
+const getAge =(person) =>person.age;
+const student ={name:'Tanzid',age:20}
+const age =getAge(student);
+console.log(age)
