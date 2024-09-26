5e1be2b2798a0adc1302f9e039accb5ade629f30
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-26 22:58:33 +0600

    array max number

--- spride.js
+++ spride.js
@@ -1,2 +1,6 @@
-const max =Math.max(54.95,52,852,89,63,85,25);
-console.log(max)
+const max = Math.max(54.95, 52, 852, 89, 63, 85, 25);
+// console.log(max)
+
+const numbers = [45, 65, 15, 98, 45, 14, 56, 75, 14, 53];
+const maxNumber =Math.max(...numbers);
+console.log(maxNumber)
