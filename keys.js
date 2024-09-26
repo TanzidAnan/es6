e640faf6374a7164cd60839ee0d13512f5ad9a6a
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 01:16:34 +0600

    keys

--- keys.js
+++ keys.js
@@ -2,5 +2,8 @@ const glass = { name: 'glass', color: 'red', price: 15, isCline: true };
 // console.log(glass)
 const keysss =Object.keys(glass);
 const valuse =Object.values(glass);
-console.log(keysss,);
-console.log(valuse)
+// console.log(keysss,);
+// console.log(valuse)
+
+const point =Object.entries(glass);
+// console.log(point)
