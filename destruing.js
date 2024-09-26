235871e83c702e43a65669660ae0b536fd64efa3
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 00:21:52 +0600

    key valuse

--- destruing.js
+++ destruing.js
@@ -1,17 +1,28 @@
-const anota ={
-    name:'ananta',
+const anota = {
+    name: 'ananta',
     age: 45,
-    phone:'017545454',
-    money:454545,
+    phone: '017545454',
+    money: 454545,
 
 }
 
-const {phone} =anota
+const { phone } = anota
 
 // const phone =anota.phone;
-console.log(phone)
-console.log(phone)
-console.log(phone)
-console.log(phone)
-console.log(phone)
-console.log(phone)
+// console.log(typeof phone)
+// console.log(phone)
+// console.log(phone)
+// console.log(phone)
+// console.log(phone)
+// console.log(phone)
+
+const numbers = [45, 50];
+const [fast, secdion] = numbers;
+const [x, y] = [45, 62];
+
+function doubleThem(a, b) {
+    return [a * 2, b * 2]
+}
+
+const [a,b] =doubleThem(10,20);
+console.log(a,b)
---
+++ keys.js
@@ -0,0 +1,6 @@
+const glass = { name: 'glass', color: 'red', price: 15, isCline: true };
+// console.log(glass)
+const keysss =Object.keys(glass);
+const valuse =Object.values(glass);
+console.log(keysss,);
+console.log(valuse)
