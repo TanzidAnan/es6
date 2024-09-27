8b98571d1d7a297b3818a6d0d7890799fb99a440
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 22:57:47 +0600

    access

--- explore-more-es6/change.js
+++ explore-more-es6/change.js
@@ -1,5 +1,14 @@
 const data = [{ id: 1, name: 'abule', address: 'Dhaka' }];
-console.log(data[0].address)
+// console.log(data[0].address)
 // for(let da of data){
 //     console.log(da.address)
 // }
+
+const pruduct = {
+    count: 5000,
+    data: [
+        { id: 1, name: 'lenovo', price: 65000 },
+        { id: 2, name: 'apple', price: 10000 }
+    ]
+}
+console.log(pruduct.data[1].price)
