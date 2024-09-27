0b5c958ce50bc5bf2bc670596a9c94dbfc4cffce
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 23:27:29 +0600

    map funcation

--- explore-more-es6/map.js
+++ explore-more-es6/map.js
@@ -12,5 +12,7 @@ const numbers = [46, 63, 45, 98, 69, 89, 69, 74, 69, 63, 37];
 const double = x => x * 2;
 
 const output = numbers.map(double)
+const output2 = numbers.map(x => x * 2)
 
-console.log(output)
+// console.log(output)
+console.log(output2)
