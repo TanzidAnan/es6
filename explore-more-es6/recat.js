5eb127071569912020af1da97df80d47077b4ce0
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 22:44:24 +0600

    object

--- explore-more-es6/recat.js
+++ explore-more-es6/recat.js
@@ -1,2 +1,5 @@
 const [fast, secdion, ...others] = [14, 25, 36, 85, 36, 74, 36];
-console.log(others)
+// console.log(others)
+
+const {name,...time} ={name:'tanzid',age:25,isArray:true};
+console.log(time)
