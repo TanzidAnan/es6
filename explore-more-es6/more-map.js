a6a8317f600bba4ea75178f3df29f4bec93554f4
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 23:34:55 +0600

    array length

--- explore-more-es6/more-map.js
+++ explore-more-es6/more-map.js
@@ -1,3 +1,7 @@
 const numbers = [5, 6, 7, 8, 9, 5, 5];
 const doubled = numbers.map(num => num * 2);
-console.log(doubled)
+// console.log(doubled)
+
+const firends =['Tanzid','Marzan','Anan'];
+const length =firends.map(firend => firend.length);
+console.log(length)
