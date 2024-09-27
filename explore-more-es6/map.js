b3c074a151877cd2af98f3187b7be6423d14bdb3
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 23:14:09 +0600

    doubled numbers

---
+++ explore-more-es6/map.js
@@ -0,0 +1,9 @@
+const numbers = [45, 63, 45, 98, 69, 89, 69, 74, 69, 63, 37];
+
+const doubled =[];
+for(const number of numbers){
+    const double =number*2;
+    doubled.push(double);
+}
+
+console.log(doubled)
