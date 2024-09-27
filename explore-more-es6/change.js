1cdde40a520cf4ec6611c3e0bf95a0f4b565c0e0
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-27 22:54:17 +0600

    index array

--- explore-more-es6/change.js
+++ explore-more-es6/change.js
@@ -1,5 +1,5 @@
 const data = [{ id: 1, name: 'abule', address: 'Dhaka' }];
-
-for(let da of data){
-    console.log(da.address)
-}
+console.log(data[0].address)
+// for(let da of data){
+//     console.log(da.address)
+// }
