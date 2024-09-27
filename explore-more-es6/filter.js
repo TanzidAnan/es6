2a385b8d6dce1b5c43c7b38f07b6c3a2a9744ae4
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-28 01:35:06 +0600

    filter

---
+++ explore-more-es6/filter.js
@@ -0,0 +1,3 @@
+const numbers =[12,4,65,12,32,45,55];
+const filter =numbers.filter(p=>p>12);
+console.log(filter)
--- explore-more-es6/forEach.js
+++ explore-more-es6/forEach.js
@@ -1,2 +1,4 @@
 const numbers =[12,4,65,12,32,45,55];
-const number =numbers.forEach(n => console.log(n))
+// const number =numbers.forEach(n => console.log(n))
+
+co
