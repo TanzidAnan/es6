9bb42b7da75bbc34858eb9422c8e9c6d9cc42afb
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-28 23:36:49 +0600

    array event

--- explore-more-es6/filter.js
+++ explore-more-es6/filter.js
@@ -7,5 +7,6 @@ const firend =firends.filter(f=> f.length >5);
 // console.log(firend);
 
 const number =[12,25,45,20,30,25,14,60,41];
-const nun =number.filter(n => n>20);
+// const nun =number.filter(n => n>20);
+const nun =number.filter(n=> n%2 ===0)
 console.log(nun)
