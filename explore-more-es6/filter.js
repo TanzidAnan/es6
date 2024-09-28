9e612f2cdd607ef01986f53cb2389a20181e5b23
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-28 09:15:01 +0600

    filter

--- explore-more-es6/filter.js
+++ explore-more-es6/filter.js
@@ -1,3 +1,7 @@
 const numbers =[12,4,65,12,32,45,55];
 const filter =numbers.filter(p=>p>12);
-console.log(filter)
+console.log(filter);
+
+const firends =['Tanzid','Marzan','Anan'];
+const firend =firends.filter(f=> f.length >5);
+console.log(firend)
