3b5eaa9b2cbb61f98a3a74df5783bf98e7e048b1
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-28 23:21:22 +0600

    array

--- explore-more-es6/forEach.js
+++ explore-more-es6/forEach.js
@@ -1,5 +1,8 @@
-const numbers =[12,4,65,12,32,45,55];
+const numbers = [12, 4, 65, 12, 32, 45, 55];
 // const number =numbers.forEach(n => console.log(n))
 
-const obj ='Tanzidananan';
-const fastName =obj.map(n=>console.log(n))
+
+const array = [52, 36, 96, 78, 10, 25, 14, 36, 52, 10, 12, 15, 63, 45,]
+
+const fastName = array.map(n => n >= 15);
+console.log(fastName)
