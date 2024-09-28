f499cfac28e9ee3befc2c093f3d71999a50bda25
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-29 00:04:02 +0600

    reduce funcation

--- explore-more-es6/array-of-object.js
+++ explore-more-es6/array-of-object.js
@@ -1,8 +1,13 @@
-const products =[
-    {id:1,name:'apple',price:45000},
-    {id:2,name:'oppo',price:14000},
-    {id:3,name:'relmi',price:5000},
-    {id:4,name:'redmi',price:20000}
+const products = [
+    { id: 1, name: 'apple', price: 45000 },
+    { id: 2, name: 'oppo', price: 14000 },
+    { id: 3, name: 'relmi', price: 5000 },
+    { id: 4, name: 'redmi', price: 20000 }
 ]
-const name =products.map(product => product.name);
+const name = products.map(product => product.name);
+const price = products.map(product => product.price);
 console.log(name)
+console.log(price)
+
+const p = products.reduce((a, b) => a + b.price, 0);
+console.log(p)
