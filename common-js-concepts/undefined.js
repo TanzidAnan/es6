ae211c1a2582dbed1b5221a2b005743a7411efdb
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-29 23:49:09 +0600

    undefined 5

--- common-js-concepts/undefined.js
+++ common-js-concepts/undefined.js
@@ -8,16 +8,19 @@ const result = second();
 // console.log(result)
 
 function third(a, b) {
-    const totle = a+ b;
+    const totle = a + b;
 }
-const result1 =third(2);
+const result1 = third(2);
 // console.log(result)
 
-function noNagative(a,b){
-    if(a<0 ||b<0){
+function noNagative(a, b) {
+    if (a < 0 || b < 0) {
         return
     }
-    return a+b
+    return a + b
 }
-const totle =noNagative(2,-2);
-console.log(totle)
+const totle = noNagative(2, -2);
+// console.log(totle)
+
+const fifth = { id: 5, name: 'Tanzid', age: 45 };
+console.log(fifth.time);
