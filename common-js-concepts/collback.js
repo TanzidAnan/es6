9ff0c957a5417e3fccbed9e1adc1d5158c6c2e95
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-30 11:41:15 +0600

    collback funcation

--- common-js-concepts/collback.js
+++ common-js-concepts/collback.js
@@ -6,4 +6,9 @@ function greetingHenler(name){
     console.log('good morning,',name)
 }
 
+function greetingEvening(name){
+     console.log('good Evening',name)
+}
+
 greeting(greetingHenler,'Tanzid')
+greeting(greetingEvening, "marzan")
